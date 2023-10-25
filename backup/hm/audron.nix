e48366abe55c1c89caaf3cf046bad58c418ec0db
Tommy Breslein <tommy.breslein@protonmail.com>
2023-10-25 14:31:38 +0200

    clean up

--- backup/hm/audron.nix
+++
@@ -1,4 +0,0 @@
-{ config, pkgs, user_name, mk_config, ... }: {
-  imports = [ ./home.nix ./linux.nix ];
-  home = { packages = [ pkgs.steam ]; };
-}
--- backup/hm/mac.nix
+++
@@ -1,4 +0,0 @@
-{ config, pkgs, user_name, mk_config, ... }: {
-  imports = [ ./home.nix ];
-  home = { packages = [ pkgs.pyenv ]; };
-}
