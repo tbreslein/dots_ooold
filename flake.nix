b19d8e5aca8965454234fa3d9840c106c38bece5
Tommy Breslein <tommy.breslein@protonmail.com>
2023-10-25 13:39:11 +0200

    start small

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "nixos+hm flake";
 
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+    trusted-users = [ "tommy" ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
