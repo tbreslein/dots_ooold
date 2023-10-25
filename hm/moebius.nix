1f4399ec111116e28114890ad6c48a96182946eb
Tommy Breslein <tommy.breslein@protonmail.com>
2023-10-25 22:40:08 +0200

    fix kanshi

--- hm/moebius.nix
+++ hm/moebius.nix
@@ -8,9 +8,7 @@
           outputs = [{
             criteria = "DP-3";
             mode = "3440x1440@144.000";
-            adaptiveSync = true;
           }];
-          exec = "wlsunset && swww init";
         };
       };
     };
