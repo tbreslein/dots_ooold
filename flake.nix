ebd681dbab935aaeadaf5ef3a10c4eac3ef0c13d
Tommy Breslein <tommy.breslein@protonmail.com>
2023-10-25 13:58:55 +0200

    basic hm setup

--- flake.nix
+++ flake.nix
@@ -16,7 +16,15 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
+    let
+      user_name = "tommy";
+      mk_config = config: name: {
+        source = config.lib.file.mkOutOfStoreSymlink
+          "${config.home.homeDirectory}/dots/config${name}";
+        target = "${config.home.homeDirectory}/.config/${name}";
+      };
+    in {
     nixosConfigurations = {
       moebius = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -24,6 +32,14 @@
           ./nixos/configuration.nix
           ./nixos/moebius.nix
           ./nixos/moebius-hardware.nix
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.extraSpecialArgs = { inherit user_name mk_config; };
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.tommy = import ./hm/home.nix;
+          }
         ] ;
       };
     };
