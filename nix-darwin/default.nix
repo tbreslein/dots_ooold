338c289f4baf90750703eb0683b022f776bf686d
Tommy Breslein <tommy.breslein@protonmail.com>
2023-11-01 13:14:32 +0100

    basic system setup

--- nix-darwin/default.nix
+++ nix-darwin/default.nix
@@ -1,4 +1,35 @@
 { pkgs, ... }: {
   programs.zsh.enable = true;
-  environment.shells = with pkgs; [ bash zsh ];
+  environment = {
+    shells = with pkgs; [ bash zsh ];
+    loginShell = pkgs.zsh;
+  };
+  nix = {
+    useDaemon = true;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+  systemPackages = [ pkgs.coreutils ];
+  system = {
+    keyboard = {
+      enableKeyMapping = true;
+      remapCapsLockToEscape = true;
+    };
+    defaults = {
+      finder = {
+        AppleShowAllExtensions = true;
+        _FXShowPosixPathInTitle = true;
+      };
+      dock.autohide = true;
+      NSGlobalDomain = {
+        AppleShowAllExtensions = true;
+        InitialKeyRepeat = 14;
+      };
+    };
+  };
+  fonts = {
+    fontDir.enable = true;
+    fonts = [ (pkgs.nerdfonts.override { fonts = [ "Meslo" "Hack" ]; }) ];
+  };
 }
