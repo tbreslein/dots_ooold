21b17eb2c7aa11a973827841d232372dfa4d09de
Tommy Breslein <tommy.breslein@protonmail.com>
2023-11-01 13:40:20 +0100

    maybe I need to force that value?

--- hm/mac.nix
+++ hm/mac.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, userConfig, ... }: {
+{ config, pkgs, lib, userConfig, ... }: {
   # imports = [ ./home.nix ];
   home = {
-    username = userConfig.name;
-    stateVersion = "23.05";
-    homeDirectory = "/Users/${userConfig.name}";
+    # username = userConfig.name;
+    # stateVersion = "23.05";
+    homeDirectory = lib.mkForce "/Users/${userConfig.name}";
     packages = [
       (pkgs.writeShellScriptBin "up" ''
         #!/usr/bin/env zsh
