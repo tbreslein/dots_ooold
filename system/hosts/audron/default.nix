d9bd34210c8a556b3d3e4194a272f26c9bc3246d
Tommy Breslein <tommy.breslein@protonmail.com>
2023-11-08 22:13:44 +0100

    fix sddm

--- system/hosts/audron/default.nix
+++ system/hosts/audron/default.nix
@@ -1,13 +1,18 @@
-{ config, pkgs, ... }:
+{ userConfig, ... }:
 
 {
   imports = [ ./hardware-configuration.nix ];
   boot = {
-    initrd.luks.devices."luks-f82f08c3-4d51-4a0e-a78d-3d3ace234663".device = "/dev/disk/by-uuid/f82f08c3-4d51-4a0e-a78d-3d3ace234663";
+    initrd.luks.devices."luks-f82f08c3-4d51-4a0e-a78d-3d3ace234663".device =
+      "/dev/disk/by-uuid/f82f08c3-4d51-4a0e-a78d-3d3ace234663";
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
     };
   };
-  networking.hostName = "audron"; # Define your hostname.
+  networking.hostName = "audron";
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = userConfig.name;
+  };
 }
--- system/linux-desktop/sddm-themes/sugar-dark.nix
+++ system/linux-desktop/sddm-themes/sugar-dark.nix
@@ -7,7 +7,8 @@
 #    sha256 = "rTE57xA9FD6AuUCRH3HKJhXDNwm5fu4WMBeW9ocUM+A=";
 #  };
 #in
-pkgs.stdenv.mkDerivation {
+let image = ./penguin.jpg;
+in pkgs.stdenv.mkDerivation {
   name = "sddm-sugar-candy";
   src = pkgs.fetchFromGitHub {
     owner = "MarianArlt";
@@ -15,15 +16,14 @@ pkgs.stdenv.mkDerivation {
     rev = "ceb2c455663429be03ba62d9f898c571650ef7fe";
     sha256 = "flOspjpYezPvGZ6b4R/Mr18N7N3JdytCSwwu6mf4owQ=";
   };
+  # installPhase = ''
+  #   mkdir -p $out
+  #   cp -R ./* $out/
+  # '';
   installPhase = ''
     mkdir -p $out
     cp -R ./* $out/
+    rm $out/Background.jpg
+    cp -r ${image} $out/Background.jpg
   '';
-  #installPhase = ''
-  #  mkdir -p $out
-  #  cp -R ./* $out/
-  #  #cd $out/
-  #  #rm Background.jpg
-  #  #cp -r ${image} $out/Background.jpg
-  #'';
 }
