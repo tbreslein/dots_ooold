bd46376b7779af6d1d5a1fbd0bd6768637130cc3
Tommy Breslein <30618763+tbreslein@users.noreply.github.com>
2023-11-21 18:23:54 +0100

    darwin needs zsh...

--- modules/cli/default.nix
+++ modules/cli/default.nix
@@ -46,6 +46,7 @@ in {
       direnv = {
         enable = true;
         enableBashIntegration = true;
+        enableZshIntegration = true;
         nix-direnv.enable = true;
       };
       eza = {
@@ -55,6 +56,7 @@ in {
       fzf = {
         enable = true;
         enableBashIntegration = true;
+        enableZshIntegration = true;
       };
       git = {
         enable = true;
@@ -70,30 +72,53 @@ in {
           };
         };
       };
-      # starship = {
-      #   enable = true;
-      #   enableBashIntegration = true;
-      #   settings = {
-      #     character = {
-      #       success_symbol = "[λ](bold yellow)";
-      #       error_symbol = "[λ](bold red)";
-      #       vicmd_symbol = "[λ](bold green)";
-      #     };
-      #     directory = {
-      #       truncate_to_repo = true;
-      #       truncation_symbol = ".../";
-      #       style = "bold blue";
-      #     };
-      #     username.format = "[$user]($style)@";
-      #     hostname.format = "[$ssh_symbol$hostname]($style): ";
-      #     format =
-      #       "$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$line_break$username$hostname$nix_shell$battery$character";
-      #     nix_shell.format = "[$symbol]($style)";
-      #   };
-      # };
+      starship = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        settings = {
+          character = {
+            success_symbol = "[λ](bold yellow)";
+            error_symbol = "[λ](bold red)";
+            vicmd_symbol = "[λ](bold green)";
+          };
+          directory = {
+            truncate_to_repo = true;
+            truncation_symbol = ".../";
+            style = "bold blue";
+          };
+          username.format = "[$user]($style)@";
+          hostname.format = "[$ssh_symbol$hostname]($style): ";
+          format =
+            "$directory$vcsh$fossil_branch$git_branch$git_commit$git_state$git_metrics$git_status$hg_branch$line_break$username$hostname$nix_shell$battery$character";
+          nix_shell.format = "[$symbol]($style)";
+        };
+      };
       zoxide = {
         enable = true;
         enableBashIntegration = true;
+        enableZshIntegration = true;
+      };
+      zsh = {
+        enable = true;
+        enableCompletion = false;
+        enableAutosuggestions = false;
+        syntaxHighlighting.enable = true;
+        initExtra = ''
+          export KEYTIMEOUT=1
+          export DISABLE_AUTO_TITLE=true
+          bindkey -v
+          setopt extendedglob nomatch menucomplete
+          unsetopt BEEP
+          stty stop undef # disable ctrl-s freezing the terminal
+          zle_highlight=('paste:none') # stop highlighting pasted text
+        '';
+        history = {
+          ignoreAllDups = true;
+          ignoreSpace = true;
+          save = 1000;
+          size = 1000;
+        };
       };
     };
   };
