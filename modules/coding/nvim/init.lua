f5b9848ce6baa1e37401ad1660c2f64698555dea
Tommy Breslein <tommy.breslein@protonmail.com>
2024-02-13 22:39:50 +0100

    nvim/add some plugins

--- modules/coding/nvim/init.lua
+++ modules/coding/nvim/init.lua
@@ -58,6 +58,33 @@ require("lazy").setup({
         end,
     },
     {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            { "rcarriga/nvim-notify", opts = { background_colour = "#000000" } },
+        },
+        opts = {
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
+            -- add any options here
+        },
+    },
+    {
         "nvim-treesitter/nvim-treesitter",
         dependencies = { "windwp/nvim-ts-autotag", "nvim-treesitter/nvim-treesitter-context" },
         build = ":TSUpdate",
@@ -72,7 +99,6 @@ require("lazy").setup({
         end,
     },
     { "numToStr/Comment.nvim", opts = {} },
-    { "j-hui/fidget.nvim", opts = {} },
     "mbbill/undotree",
     {
         "nvim-lualine/lualine.nvim",
@@ -96,6 +122,12 @@ require("lazy").setup({
             },
         },
     },
+    {
+        "NeogitOrg/neogit",
+        dependencies = { "sindrets/diffview.nvim", "nvim-telescope/telescope.nvim" },
+        opts = {},
+    },
+    { "nvim-pack/nvim-spectre", opts = {} },
 
     -- navigation
     {
@@ -268,6 +300,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
         bufmap("n", "<leader>A", "<cmd>lua vim.lsp.buf.code_action()<cr>")
     end,
 })
+kmap("n", "<leader>gg", require("neogit").open)
 
 -- >>> LSP
 local lspconfig = require("lspconfig")
@@ -307,8 +340,8 @@ cmp.setup({
         ["<c-j>"] = cmp.mapping.select_next_item(select_opts),
         ["<c-k>"] = cmp.mapping.select_prev_item(select_opts),
         ["<c-l>"] = cmp.mapping.confirm({ select = true }),
-        ["<c-m>"] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
-        ["<c-.>"] = cmp.mapping(cmp.mapping.scroll_docs(4)),
+        ["<c-n>"] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
+        ["<c-m>"] = cmp.mapping(cmp.mapping.scroll_docs(4)),
     }),
     enabled = function() return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" end,
     formatting = {
