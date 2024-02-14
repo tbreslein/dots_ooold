5b6084dd61b998a321a8375d576a8d7c72553641
Tommy Breslein <tommy.breslein@anacision.de>
2024-02-14 15:46:47 +0100

    nvim/clean up

--- modules/coding/nvim/init.lua
+++ modules/coding/nvim/init.lua
@@ -62,18 +62,16 @@ require("lazy").setup({
         event = "VeryLazy",
         dependencies = {
             "MunifTanjim/nui.nvim",
-            { "rcarriga/nvim-notify", opts = { background_colour = "#000000" } },
+            { "rcarriga/nvim-notify", opts = { background_colour = "#000000", render = "compact" } },
         },
         opts = {
             lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
                 override = {
                     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
                     ["vim.lsp.util.stylize_markdown"] = true,
                     ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
                 },
             },
-            -- you can enable a preset for easier configuration
             presets = {
                 bottom_search = true, -- use a classic bottom cmdline for search
                 command_palette = true, -- position the cmdline and popupmenu together
@@ -81,7 +79,6 @@ require("lazy").setup({
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = false, -- add a border to hover docs and signature help
             },
-            -- add any options here
         },
     },
     {
